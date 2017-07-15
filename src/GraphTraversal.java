643230d186ee52dc76271b1c5239c8ccd013baa0
julianljk <julianljk@gmail.com>
2017-07-15 13:59:06 -0500

    topological sorting

--- src/GraphTraversal.java
+++ src/GraphTraversal.java
@@ -3,8 +3,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 
 import graph.*;
 
--- src/TopologicalSorting.java
+++ src/TopologicalSorting.java
@@ -1,7 +1,9 @@
+import java.util.HashMap;
+import java.util.Stack;
+
 import graph.*;
 
 public class TopologicalSorting {
-	@SuppressWarnings("unchecked")
 	public static void main (String [] args) {
 		DirectedGraph <Integer> dag = new DirectedGraph <Integer>();
 		for(int i = 0; i < 6; i++) {
@@ -14,6 +16,8 @@ public class TopologicalSorting {
 		dag.addEdge(new Edge <Integer>(dag.getVertice(2),dag.getVertice(3)));
 		dag.addEdge(new Edge <Integer>(dag.getVertice(3),dag.getVertice(1)));
 		printGraph(dag);
+		System.out.println("Topological Sort Result: ");
+		topologicalSort(dag);
 	}
 	public static void printGraph(DirectedGraph <Integer> dag) {
 		for(Vertex<Integer> v: dag.getVertices().values()) {
@@ -26,5 +30,31 @@ public class TopologicalSorting {
 				System.out.println("    <- " + neighbor.getValue());
 			}
 		}
+	}
+	public static void topologicalSort(DirectedGraph <Integer> dag) { //O(V + E)
+		Stack <Integer> stack = new Stack <Integer>(); 
+		HashMap <Integer, Boolean> visited = new HashMap <Integer, Boolean> (); 
+		
+		for(Integer i: dag.getVertices().keySet()) { //O(V)
+			visited.put(i, false); 
+		}
+		for(Vertex <Integer> v: dag.getVertices().values()) { //O(V)
+			if(!visited.get(v.getValue())) {
+				topologicalSortHelper(visited, stack, (DirectedVertex <Integer>) v);
+			}
+		}
+		while(!stack.isEmpty()) {
+			System.out.print(stack.pop() + " -> "); 
+		}
+		
+	}
+	private static void topologicalSortHelper(HashMap <Integer, Boolean> visited, Stack <Integer> stack, DirectedVertex <Integer> curr) {
+		visited.put(curr.getValue(), true);
+		for(DirectedVertex <Integer> v: curr.getOutgoing()) { //O(E)
+			if(!visited.get(v.getValue())) {
+				topologicalSortHelper(visited, stack, v);
+			}
+		}
+		stack.push(curr.getValue());
 	}
 }
