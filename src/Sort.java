5354b9983cbb07e5e67cbcadc2d84d0dd15a5455
Julian Leong <jleong@fac.local>
2017-07-20 13:46:08 -0500

    radix sort

--- src/Sort.java
+++ src/Sort.java
@@ -1,17 +1,15 @@
+import java.util.Arrays;
+
 public class Sort {
     public static void main (String [] args) {
-        int [] arr = new int [200]; 
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (Math.random() * 100);     
-        }
-        for(int i: arr){
-        	System.out.print(i + ", ");
-        }
-        arr = countingSort(arr);
-        System.out.println();
-        for(int i: arr){
-        	System.out.print(i + ", ");
-        }
+//        int [] arr = new int [200]; 
+//        for(int i = 0; i < arr.length; i++) {
+//            arr[i] = (int) (Math.random() * 100);     
+//        }
+    	int [] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+        printArray(arr);
+        radixSort(arr);
+        printArray(arr);
     }
     public static int [] countingSort(int [] arr) { //array holds values from range 0 - 100
         int [] count = new int [100]; 
@@ -28,5 +26,44 @@ public class Sort {
             count[arr[i]]--; 
         }
         return temp;
+    }
+    
+    public static void countingSort(int [] arr, int n, int exp, int base) { //array holds values from range 0 - 100
+        int [] count = new int [base];
+        int [] temp = new int [n]; 
+        Arrays.fill(count,0);
+        for(int i = 0; i < n; i++) {
+            count[(arr[i]/exp)%base]++;     
+        }
+        for(int i = 1; i < base; i++) {
+            count[i] += count[i - 1];    
+        }
+        for(int i = n -1; i >= 0; i--) {
+            temp[count[(arr[i]/exp)%base] - 1] = arr[i]; 
+            count[(arr[i]/exp)%base]--; 
+        }
+        for(int i = 0; i < n; i++) {
+            arr[i] = temp[i];    
+        }
+    }
+    public static int getMax(int arr[])
+    {
+        int mx = arr[0];
+        for (int i = 1; i < arr.length; i++)
+            if (arr[i] > mx)
+                mx = arr[i];
+        return mx;
+    }
+    public static void radixSort(int [] arr) {
+        int max = getMax(arr); 
+        for(int i = 1; max/i > 0 ; i *=10) {
+            countingSort(arr, arr.length, i, 10);
+        }
+    }
+    public static void printArray(int [] arr) {
+        for(int i: arr) {
+            System.out.print(i + ", ");     
+        }
+        System.out.println(); 
     }
 }
